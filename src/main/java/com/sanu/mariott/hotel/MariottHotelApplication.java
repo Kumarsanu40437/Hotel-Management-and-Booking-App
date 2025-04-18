6a6857061b64c6395b5e29bd280a64d6e11dd6ab
Kumarsanu40437 <kumarsanu40437@gmail.com>
2025-04-18 18:08:21 +0530

    Project setup

---
+++ src/main/java/com/sanu/mariott/hotel/MariottHotelApplication.java
@@ -0,0 +1,13 @@
+package com.sanu.mariott.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariottHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariottHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanu/mariott/hotel/MariottHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanu.mariott.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariottHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
